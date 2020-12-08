a82972716db2d94f82398e844917585e51ca88da
alejandromunhiz <38499481+alejandromunhiz@users.noreply.github.com>
2020-12-08 18:47:57 +0100

    Update simpletun.c

--- simpletun.c
+++ simpletun.c
@@ -361,8 +361,8 @@ int main(int argc, char *argv[]) {
   const unsigned char aes_key[] = {0x00,0x11,0x22,0x33,0x44,0x55,0x66,0x77,0x88,0x99,0xAA,0xBB,0xCC,0xDD,0xEE,0xFF};
   char buffer_encriptado[BUFSIZE];
   AES_KEY enc_key, dec_key;
-//  AES_set_encrypt_key(aes_key, sizeof(aes_key)*8, &enc_key);
-//  AES_set_decrypt_key(aes_key, sizeof(aes_key)*8, &dec_key);
+  AES_set_encrypt_key(aes_key, sizeof(aes_key)*8, &enc_key);
+  AES_set_decrypt_key(aes_key, sizeof(aes_key)*8, &dec_key);
 		
 #endif
 
@@ -412,7 +412,6 @@ int main(int argc, char *argv[]) {
 #elif defined AES 
 /*Si al compilar definimos AES, se reiniciarán iv y dec_key y se encriptará buffer en buffer_encriptado */
   	memset(iv, 0x00, AES_BLOCK_SIZE);
-  	AES_set_encrypt_key(aes_key, sizeof(aes_key)*8, &enc_key);
 
 	/*Adaptamos el tamaño del mensaje para el AES */
 	int nread2 = nread;
@@ -468,7 +467,6 @@ int main(int argc, char *argv[]) {
 #elif defined AES
 /*Si al compilar definimos AES, se reiniciarán iv y dec_key y se desencriptará buffer_encriptado */
 	memset(iv, 0x00, AES_BLOCK_SIZE); 
-  	AES_set_decrypt_key(aes_key, sizeof(aes_key)*8, &dec_key);
   	AES_cbc_encrypt(buffer_encriptado, buffer, nread, &dec_key, iv, AES_DECRYPT);
 
 #endif
